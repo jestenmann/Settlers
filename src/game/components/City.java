80c56661a48c78eb21a2aa536406a20991bf3655
Mladen Yakimov <yakimov89@gmail.com>
2014-04-29 16:44:55 -0400

    city and settlement classes

---
+++ src/game/components/City.java
@@ -0,0 +1,14 @@
+package game.components;
+
+public class City {
+	private Corner corner;
+	
+	public City(Corner corner) {
+		this.corner = corner;
+	}
+	
+	
+	public Corner getCorner() {
+		return corner;
+	}
+}
---
+++ src/game/components/Settlement.java
@@ -0,0 +1,15 @@
+package game.components;
+
+public class Settlement {
+	private Corner corner;
+	
+	public Settlement(Corner corner) {
+		this.corner = corner;
+	}
+	
+	
+	public Corner getCorner() {
+		return corner;
+	}
+	
+}
